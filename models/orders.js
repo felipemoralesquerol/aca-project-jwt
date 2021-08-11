c1fba7cd82fb9d564e0f32608b519968e351272c
Felipe Morales <felipe.morales.querol@gmail.com>
2021-08-11 09:16:32 -0300

    Fix de orders

--- models/orders.js
+++ models/orders.js
@@ -3,6 +3,8 @@ var orders = [];
 orders.push({ id: 1, onwer: "Juan Pablo" });
 orders.push({ id: 2, onwer: "Felipe" });
 orders.push({ id: 3, onwer: "Sergio" });
+orders.push({ id: 4, onwer: "Diego" });
+
 
 
 module.exports = orders;
